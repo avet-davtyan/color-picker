76ca9cca3c2f47fcfb86847dc16a78cab0c0d27b
avet-davtyan <avetdavtyan04@gmail.com>
2024-07-16 15:47:26 +0400

    add ability to laod an image

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,55 @@
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
 import Canvas from './Canvas';
 
 function App() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [imageFile, setImageFile] = useState<Blob | null>(null);
+
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const files: FileList | null = event.target.files;
+        if (!files) return;
+        setImageFile(files[0]);
+    };
+
+    const drawImage = (file: Blob) => {
+        const canvas: HTMLCanvasElement | null = canvasRef.current;
+        if (!canvas) return;
+        const context: CanvasRenderingContext2D | null =
+            canvas.getContext('2d');
+        if (!context) return;
+
+        const reader = new FileReader();
+
+        reader.onload = event => {
+            const img = new Image();
+            img.onload = () => {
+                canvas.width = img.width;
+                canvas.height = img.height;
+                context.drawImage(img, 0, 0);
+            };
+            if (event.target) {
+                img.src = event.target.result as string;
+            }
+        };
+
+        reader.readAsDataURL(file);
+    };
+
+    useEffect(() => {
+        if (imageFile) {
+            drawImage(imageFile);
+        }
+    }, [imageFile]);
+
     return (
         <>
-            <Canvas />
+            <input
+                type='file'
+                accept='image/*'
+                onChange={handleFileChange}
+                multiple={false}
+            />
+            <canvas ref={canvasRef} />
         </>
     );
 }
--- src/Canvas/Canvas.tsx
+++ src/Canvas/Canvas.tsx
@@ -1,5 +1,10 @@
-const Canvas = () => {
-    return <canvas />;
+import { Ref, RefObject, useEffect } from 'react';
+
+const Canvas = ({ ref }: { ref: RefObject<HTMLCanvasElement> }) => {
+    useEffect(() => {
+        const canvas: HTMLCanvasElement | null = ref.current;
+    });
+    return <canvas ref={ref} />;
 };
 
 export default Canvas;
