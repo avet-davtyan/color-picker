4e706cd1a31f20268b60732ec6b93505350be820
avet-davtyan <avetdavtyan04@gmail.com>
2024-07-16 16:42:43 +0400

    add magnifying glass effect

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Canvas from './Canvas';
 function App() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [imageFile, setImageFile] = useState<Blob | null>(null);
+    const [image, setImage] = useState<CanvasImageSource | null>(null);
 
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         const files: FileList | null = event.target.files;
@@ -25,6 +26,7 @@ function App() {
             img.onload = () => {
                 canvas.width = img.width;
                 canvas.height = img.height;
+                setImage(img);
                 context.drawImage(img, 0, 0);
             };
             if (event.target) {
@@ -35,11 +37,45 @@ function App() {
         reader.readAsDataURL(file);
     };
 
+    const zoom = (x: number, y: number, zoomScale: number) => {
+        console.log('running');
+        const canvas: HTMLCanvasElement | null = canvasRef.current;
+        if (!canvas) return;
+        const context: CanvasRenderingContext2D | null =
+            canvas.getContext('2d');
+        if (!context || !image) return;
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(image, 0, 0);
+
+        context.beginPath();
+        context.arc(x, y, 100, 0, Math.PI * 2);
+        context.clip();
+        context.drawImage(
+            image,
+            x - x / zoomScale,
+            y - y / zoomScale,
+            canvas.width / zoomScale,
+            canvas.height / zoomScale,
+            0,
+            0,
+            canvas.width,
+            canvas.height,
+        );
+        context.restore();
+    };
+
     useEffect(() => {
         if (imageFile) {
             drawImage(imageFile);
         }
     }, [imageFile]);
+
+    useEffect(() => {
+        if (image) {
+            zoom(500, 500, 1.5);
+        }
+    }, [image]);
 
     return (
         <>
@@ -49,7 +85,15 @@ function App() {
                 onChange={handleFileChange}
                 multiple={false}
             />
-            <canvas ref={canvasRef} />
+            <canvas
+                ref={canvasRef}
+                style={{
+                    width: '500px',
+                }}
+                onMouseMove={event => {
+                    console.log(event);
+                }}
+            />
         </>
     );
 }
